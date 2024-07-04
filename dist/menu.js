f1a75a734ed117ed75867844b4c59b34b8fcfd88
eonduck2 <whdtnxd@gmail.com>
2024-07-04 12:35:56 +0900

    :package: 이종수: 바벨 트랜스파일링 완료 (main.ts)

--- dist/menu.js
+++ dist/menu.js
@@ -2,7 +2,7 @@
 
 var root = document.getElementById("root");
 var anchorTags = function anchorTags(href, textNode) {
-  var result = "<a href=\"".concat(href, "\">").concat(textNode, "</a>");
+  var result = "<a href=\"#".concat(href, "\">").concat(textNode, "</a>");
   return result;
 };
 var liTags = function liTags(children) {
@@ -10,19 +10,50 @@ var liTags = function liTags(children) {
   return result;
 };
 var basicData = {
-  hi: "\u314E\u3147",
-  bye: "\u3142\u3147",
-  hello: "\uC548\uB155",
-  goodbye: "\uC798\uAC00"
+  hi: {
+    name: "\u314E\u3147",
+    backgroundColor: "red"
+  },
+  bye: {
+    name: "\u3142\u3147",
+    backgroundColor: "blue"
+  },
+  hello: {
+    name: "\uC548\uB155",
+    backgroundColor: "orange"
+  },
+  goodbye: {
+    name: "\uC798\uAC00",
+    backgroundColor: "green"
+  }
 };
 var totalElement = function totalElement(obj) {
   var result = "";
-  // liTags(anchorTags(`#${obj.hi}`, obj.hi));
-  for (var _key in obj) {
-    result += liTags(anchorTags("#".concat(_key), obj[_key]));
+  for (var key in obj) {
+    var value = obj[key];
+    result += liTags(anchorTags(key, value.name));
   }
   return result;
 };
 if (root instanceof HTMLElement) {
-  root.innerHTML = "\n<ul>\n  ".concat(totalElement(basicData), "\n</ul>\n");
+  root.innerHTML = "\n<ul>\n  ".concat(totalElement(basicData), "\n</ul>\n<main id=\"main-target\">\n</main>\n");
 }
+
+/* IMPORTANT------------------------------------------------------------------------------
+  NOTE: HTML을 대신해서 만든 HTML 작성 코드 - REACT/CSR
+*/
+
+var mainTarget = document.getElementById("main-target");
+window.onhashchange = function () {
+  var hash = window.location.hash;
+  if (mainTarget instanceof HTMLElement) {
+    var test = basicData[hash.slice(1)];
+    var div = document.createElement("div");
+    div.style.backgroundColor = test.backgroundColor;
+    div.textContent = test.name;
+    if (mainTarget.hasChildNodes()) {
+      mainTarget.removeChild(mainTarget.childNodes[0]);
+    }
+    mainTarget.appendChild(div);
+  }
+};
--- src/menu.ts
+++ src/menu.ts
@@ -1,7 +1,7 @@
 const root = document.getElementById("root");
 
 const anchorTags = (href: string, textNode: string): string => {
-  let result = `<a href="${href}">${textNode}</a>`;
+  let result = `<a href="#${href}">${textNode}</a>`;
   return result;
 };
 
@@ -11,24 +11,48 @@ const liTags = (children: string) => {
 };
 
 type TBasicData = {
-  hi: `ㅎㅇ`;
-  bye: `ㅂㅇ`;
-  hello: `안녕`;
-  goodbye: `잘가`;
-  [key: string]: string;
+  hi: {
+    name: `ㅎㅇ`;
+    backgroundColor: `red`;
+  };
+  bye: {
+    name: `ㅂㅇ`;
+    backgroundColor: `blue`;
+  };
+  hello: {
+    name: `안녕`;
+    backgroundColor: `orange`;
+  };
+  goodbye: {
+    name: `잘가`;
+    backgroundColor: `green`;
+  };
 };
 
 const basicData: TBasicData = {
-  hi: `ㅎㅇ`,
-  bye: `ㅂㅇ`,
-  hello: `안녕`,
-  goodbye: `잘가`,
+  hi: {
+    name: `ㅎㅇ`,
+    backgroundColor: `red`,
+  },
+  bye: {
+    name: `ㅂㅇ`,
+    backgroundColor: `blue`,
+  },
+  hello: {
+    name: `안녕`,
+    backgroundColor: `orange`,
+  },
+  goodbye: {
+    name: `잘가`,
+    backgroundColor: `green`,
+  },
 };
 
 const totalElement = (obj: TBasicData): string => {
   let result = "";
   for (let key in obj) {
-    result += liTags(anchorTags(`#${key}`, obj[key]));
+    const value = obj[key as keyof TBasicData];
+    result += liTags(anchorTags(key, value.name));
   }
   return result;
 };
@@ -38,9 +62,28 @@ if (root instanceof HTMLElement) {
 <ul>
   ${totalElement(basicData)}
 </ul>
+<main id="main-target">
+</main>
 `;
 }
 
 /* IMPORTANT------------------------------------------------------------------------------
   NOTE: HTML을 대신해서 만든 HTML 작성 코드 - REACT/CSR
 */
+
+const mainTarget = document.getElementById(`main-target`);
+
+window.onhashchange = () => {
+  const hash = window.location.hash;
+
+  if (mainTarget instanceof HTMLElement) {
+    let test = basicData[hash.slice(1) as keyof TBasicData];
+    let div = document.createElement("div");
+    div.style.backgroundColor = test.backgroundColor;
+    div.textContent = test.name;
+    if (mainTarget.hasChildNodes()) {
+      mainTarget.removeChild(mainTarget.childNodes[0]);
+    }
+    mainTarget.appendChild(div);
+  }
+};
